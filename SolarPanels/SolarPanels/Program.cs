360df8a9e0ec0dceb34075a47172063e85bb02b7
dbonilla <dbonilla@outcoding.com>
2020-01-07 08:57:35 -0600

    Implementation of solar panels exercise

---
+++ SolarPanels/SolarPanels/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolarPanels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca un area");
+            double area = double.Parse(Console.ReadLine());
+
+            foreach (int element in SolarPanels(area))
+            {
+                Console.WriteLine("Area del Cuadrante: " + element);
+            }
+
+            static List<int> SolarPanels(double area)
+            {
+                List<int> cuadrantes = new List<int>();
+
+                while (area >= 1)
+                {
+                    double raiz = Math.Floor(Math.Sqrt(area));
+                    double areaCuadrante = raiz * raiz;
+                    area -= areaCuadrante;
+                    cuadrantes.Add(Convert.ToInt32(areaCuadrante));
+
+                }
+
+                return cuadrantes;
+            }
+
+        }
+    }
+}
